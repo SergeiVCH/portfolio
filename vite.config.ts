ea41ce9572b0453607caa82a2950128ababdfd49
SergeiVCH <124059057+SergeiVCH@users.noreply.github.com>
2026-03-23 13:18:12 +0500

    внес изменения Настройка vite.config.ts для SEO

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
+import react, {reactCompilerPreset} from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({presets: [reactCompilerPreset()]}),
+    Sitemap({
+      // Укажи здесь свой будущий домен (например, на Vercel или Netlify)
+      hostname: 'https://https://sergei-dev.netlify.app/',
+
+      // Так как у тебя Landing Page, мы можем указать пути-якоря
+      // (хотя роботы лучше индексируют чистые пути, для SPA это полезно)
+      dynamicRoutes: ['/', '/#about', '/#tech', '/#projects', '/#contact'],
+
+      // Плагин сам создаст sitemap.xml в папке dist при сборке (pnpm build)
+      generateRobotsTxt: true, // Это создаст и robots.txt автоматически!
+    }),
   ],
 })
